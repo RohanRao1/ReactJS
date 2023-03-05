d1f7cf95725326a8f4a1fac0bfd91a5c80d07575
RohanRao1 <74089730+RohanRao1@users.noreply.github.com>
2023-03-05 13:17:35 +0530

    First component

---
+++ ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem(){
+    return (
+    <div><h2>Expense Item</h2>
+    <p>Food 10</p>
+    <p>petrol 100</p>
+    <p>movies 200</p>
+    <p>party 500</p></div>
+    )
+}
+
+export default ExpenseItem 
