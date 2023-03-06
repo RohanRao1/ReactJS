33479d131061b99124a9f791c704dae607832374
RohanRao1 <74089730+RohanRao1@users.noreply.github.com>
2023-03-07 01:26:04 +0530

    passing data to the jsx

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,18 +1,12 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance' ;
-  const expenseAmount = 500 ;
-  const locationOfExpenditure = 'Hubli' ;
-
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.location} - {props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle} - {locationOfExpenditure}</h2>
-        <div className="expense-item__price">Rs. {expenseAmount}</div>
+        <h2>{props.title} </h2>
+        <div className="expense-item__price">Rs. {props.amount}</div>
       </div>
     </div>
   );
