8b9c2f480f319e43e956fe65aab32a947f67658e
RohanRao1 <74089730+RohanRao1@users.noreply.github.com>
2023-03-20 15:08:43 +0530

    lifting up the data from child to parent

--- ExpenseForm.js
+++ ExpenseForm.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
 
-  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredTitle, setEnteredTitle] = useState('');
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
 
@@ -60,7 +60,12 @@ const ExpenseForm = () => {
       date :new Date(enteredDate)
     }
 
-    console.log(expenseData)
+    props.onSaveExpenseData(expenseData)
+
+    setEnteredTitle('')
+    setEnteredAmount('')
+    setEnteredDate('')
+    
   }
 
   return (
@@ -68,11 +73,17 @@ const ExpenseForm = () => {
       <div className="new-expense__contorls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChange} />
+          <input type="text" value={enteredTitle} onChange={titleChange} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange={amountChange} />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={amountChange}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
@@ -81,11 +92,12 @@ const ExpenseForm = () => {
             min="2019-01-01"
             max="2022-12-31"
             onChange={dateChange}
+            value={enteredDate}
           />
         </div>
       </div>
       <div className="new-expense__actions">
-        <button type="submit" >Add Expense</button>
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
