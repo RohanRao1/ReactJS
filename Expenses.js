6f5db8d23cf7ec1a5c4bd46e4de4b062c796a725
RohanRao1 <74089730+RohanRao1@users.noreply.github.com>
2023-03-22 15:10:24 +0530

    conditional rendering

--- Expenses.js
+++ Expenses.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
+//import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpenseFilter from "./ExpenseFilter";
+import ExpenseList from "./ExpenseList";
 
 const Expenses = (props) => {
   //console.log("this is expense.js");
@@ -12,26 +13,40 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
-  const filteredExpenses = props.items.filter(expense => {
-    return expense.date.getFullYear().toString() === filteredYear
-  })
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
+
+
+  
   return (
     <Card className="expenses">
       <ExpenseFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}  
+      {/* {filteredExpenses.length === 0 ? <p>no expenses</p> : filteredExpenses.map((expense) => (
+        <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date = {expense.date} />
+      ))} */}
+      <ExpenseList items={filteredExpenses} />
     </Card>
   );
 };
 
-export default Expenses;
+export default Expenses; 
+
+
+/* {filteredExpenses.length === 0 ? (
+        <p>No Items Found</p>
+      ) : (
+        filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))
+      // )} */
+/*either we can use this condition or we can use outside condition */
